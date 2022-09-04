8bc14b51a001b4625fffe081a622d11a515f2cbc
Rafael Barboza <rafael.almeida1096@gmail.com>
2022-09-04 15:08:09 -0300

    Função que retorna a quantidade de vogais num vetor de caracteres que está no main

---
+++ Atividades/contvogais.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void contvogal(char *vetor);
+
+int main(){
+    char nome[50] = "Rafael Barboza de Almeida";
+    char *p;
+
+    p = nome;
+    contvogal(p);
+
+    return 0;
+}
+
+void contvogal(char *vetor){
+    char vogais[] = "aeiouAEIOU";
+    int cont = 0;
+    int i = 0;
+
+    while (vetor[i] != '\0'){
+        for (int j = 0; j <= 10; j++){
+            if (vetor[i] == vogais[j]){
+                cont++;
+            }
+        }
+
+        i++;
+    }
+
+    printf("\nEsse vetor possui %d vogais.", cont);
+
+}
