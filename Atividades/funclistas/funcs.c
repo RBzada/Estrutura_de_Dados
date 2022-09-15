8cfb949474a804e6c445692b169fea605c8c8f6e
Rafael Barboza <rafael.almeida1096@gmail.com>
2022-09-15 20:33:09 -0300

    Atividade para desenvolver sistema para manipular listas

---
+++ Atividades/funclistas/funcs.c
@@ -0,0 +1,14 @@
+#include "funcs.h"
+#include <stdio.h>
+#include <string.h>
+
+void menu(){
+    int opcao;
+
+    printf("------------");
+    printf("\n    MENU\n");
+    printf("------------");
+    printf("\n\nDigite a operação que deseja fazer na lista: ");
+
+
+}
---
+++ Atividades/funclistas/funcs.h
@@ -0,0 +1,12 @@
+#ifndef FUNCS_H
+#define FUNCS_H
+#define N 5
+
+typedef struct{
+    int cti, ctf;
+    int dados[N];
+}L1;
+
+void menu();
+
+#endif
