57890da0f282072408dc30c141e90817328691c8
Rafael Barboza <rafael.almeida1096@gmail.com>
2022-10-01 09:41:01 -0300

    Código para manipular listas circulares

---
+++ ListasCirculares/funclistas/funcs.h
@@ -0,0 +1,27 @@
+#ifndef FUNCS_H
+#define FUNCS_H
+#define N 5
+
+typedef struct{
+    int cti, ctf;
+    int dados[N];
+}Lista;
+
+
+void menu();
+
+void criar_lista(Lista *l);
+
+void limpar_lista(Lista *l);
+
+void inserir_inicio(Lista *l, int elemento);
+
+void inserir_fim(Lista *l, int elemento);
+
+void remover_inicio(Lista *l);
+
+void remover_fim(Lista *l);
+
+void mostrar_lista(Lista *l);
+
+#endif
