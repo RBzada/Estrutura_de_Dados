4c9afc66402a95e9191eecc2040c4be6a013234a
Rafael Barboza <rafael.almeida1096@gmail.com>
2023-02-26 22:11:45 -0300

    Corrigindo atividade de mostrar tabela ASCII

--- Atividades/tabelaASCII.c
+++ Atividades/tabelaASCII.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 int main(){
+    unsigned char i;
+
     printf("  TABELA ASCII\n");
-    for (char i = 32; i < 127; i++){
+    for (i = 32; i < 255; i++){
         if (i < 100){
             printf("| (%d)  | (%c) |\n", i, i);
         }
         else{
             printf("| (%d) | (%c) |\n", i, i);
-        }
-        
+        } 
     }
+    printf("| (%d) | (%c) |\n", i, i);
 }
