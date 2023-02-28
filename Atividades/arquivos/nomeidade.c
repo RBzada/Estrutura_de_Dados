52c0cd3cc9d2298e670625e6a4620b876da12091
Rafael Barboza <rafael.almeida1096@gmail.com>
2023-02-28 16:55:52 -0300

    Pasta para colocar as atividades sobre arquivos

---
+++ Atividades/arquivos/nomeidade.c
@@ -0,0 +1,29 @@
+// Programa que ler primeiro nome e idade de n pessoas de um arquivo txt
+
+#include <stdio.h>
+
+int main(){
+    FILE *arq; // Ponteiro para o arquivo que vai ser lido
+    FILE *arq1; // Ponteiro para o arquivo que vai ser escrito
+    char nome[30];
+    int num = -1;
+
+    arq = fopen("nomeidade.txt", "r");
+    arq1 = fopen("nomeidade1.txt", "w");
+
+    if (!arq && arq1){
+        printf("Nao abriu");
+        return 0;
+    }
+
+    fscanf(arq, "%s %d", nome, &num);
+    fprintf(arq1,"%s %d\n", nome, num);
+    while (!feof(arq)){
+        fscanf(arq, "%s %d", nome, &num);
+        fprintf(arq1,"%s %d\n", nome, num);
+    }
+
+    fclose(arq);
+    fclose(arq1);
+    return 0;
+}
