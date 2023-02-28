062a3fde71dc87f97af8a70f7065025cd0649418
Rafael Barboza <rafael.almeida1096@gmail.com>
2023-02-28 14:01:16 -0300

    Programa com função que retorna o numero de raizes reais e distintas da equação de segundo grau. As raízes são armazenadas em variáveis do main

---
+++ Atividades/calcraiz.c
@@ -0,0 +1,43 @@
+// Programa para calcular as raízes de uma equação do segundo grau
+
+#include <stdio.h>
+#include <math.h>
+
+int raizes(float a, float b, float c, float *x1, float *x2);
+
+int main(){
+    float coefa = 3, coefb = 0, coefc = -12, xp1 = -1, xp2 = -1;
+
+    raizes(coefa, coefb, coefc, &xp1, &xp2);
+    
+    if (raizes(coefa, coefb, coefc, &xp1, &xp2) == 2){
+        printf("\nEssa equacao possui 2 raizes sendo elas: %.2f e %.2f", xp1, xp2);
+    }else if(raizes(coefa, coefb, coefc, &xp1, &xp2) == 1){
+        printf("\nEssa equacao possui 1 raiz sendo ela %.2f", xp1);
+    }else{
+        printf("\nEssa equacao nao possui raiz");
+    }
+
+    return 0;
+}
+
+
+int raizes(float a, float b, float c, float *x1, float *x2){
+    float delta;
+
+    delta = pow(b, 2) - 4 * a * c;
+
+    if (delta > 0){
+        *x1 = (-b + sqrt(delta)) / (2 * a);
+        *x2 = (-b - sqrt(delta)) / (2 * a);
+
+        return 2;
+    }else if (delta == 0){
+        *x1 = (-b + sqrt(delta)) / (2 * a);
+        *x2 = *x1;
+
+        return 1;
+    }else{
+        return 0;
+    }
+}
