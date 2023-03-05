fbe9c6e743f8708e4c29a02ce49836547f89e219
Rafael Barboza <rafael.almeida1096@gmail.com>
2023-03-04 22:55:15 -0300

    Atividade para contara a quantidade de vogais num vetor de caracteres

--- Atividades/contvogais.c
+++ Atividades/contvogais.c
@@ -1,8 +1,8 @@
-// Programa para contar a quantidade de vogais que um vetor de char possui
+// Programa para calcular quantas vogais um vetor de char possui possui
 
 #include <stdio.h>
 
-void contvogal(char *vetor);
+void contvogal(char *str);
 
 int main(){
     char nome[50] = "Rafael Barboza de Almeida";
@@ -14,18 +14,16 @@ int main(){
     return 0;
 }
 
-void contvogal(char *vetor){
+void contvogal(char *str){
     char vogais[] = "aeiouAEIOU";
-    int cont = 0;
-    int i = 0;
+    int i = 0,cont = 0;
 
-    while (vetor[i] != '\0'){
+    while (str[i] != '\0'){
         for (int j = 0; j <= 10; j++){
-            if (vetor[i] == vogais[j]){
+            if (str[i] == vogais[j]){
                 cont++;
             }
         }
-
         i++;
     }
 
