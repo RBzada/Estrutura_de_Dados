43a1046809ea762ff23ff3ef50bde5411d94ecc7
Rafael Barboza <rafael.almeida1096@gmail.com>
2023-05-12 21:23:11 -0300

    Adicionando opções para usuário

--- Listas/pilha/main.c
+++ Listas/pilha/main.c
@@ -5,20 +5,55 @@
 
 int main(){
     struct Pilha *p1;
+    int continuar_operacoes = 1;
 
     criar_pilha(&p1);
 
-    mostrar_topo(p1);
+    while (continuar_operacoes == 1){ // Para continuar as operações com a pilha
+        int opcao = -1;
+    
+        menu();
+        scanf("%d", &opcao);
+        while (opcao < 1 || opcao > 5){ // Verificando se a opcao digitada é válida
+            printf("\nOpcao invalida! Tente novamente!\n");
+            menu();
+            scanf("%d", &opcao);
+        }
 
-    inserir_topo(&p1, 9);
+        if (opcao == 1){
+            esvaziar_pilha(&p1);
+            printf("\nA pilha foi limpa!\n");
+        }
 
-    inserir_topo(&p1, 10);
+        else if (opcao == 2){
+            int num; 
 
-    p1 = esvaziar_pilha(&p1);
+            printf("\nDigite o numero que deseja colocar no topo: ");
+            scanf("%d", &num);
+            inserir_topo(&p1, num);
+        }
 
-    printf("%d", p1);
+        else if (opcao == 3){
+            remover_topo(&p1);
+            printf("\nTopo removido!");
+        }
 
-    mostrar_topo(p1);
+        else if (opcao == 4){
+            mostrar_topo(p1);
+        }
 
+        printf("\nContinuar manipulando a pilha (0 para nao e 1 para sim): ");
+        scanf("%d", &continuar_operacoes);
+        while (continuar_operacoes < 0 || continuar_operacoes > 1){ // Verificando se a opcao digitada é válida
+            printf("\nOpcao invalida! Tente novamente!\n");
+            printf("\nContinuar manipulando a pilha (0 para nao e 1 para sim): ");
+            scanf("%d", &continuar_operacoes);
+        }
+
+        if (continuar_operacoes == 0){
+            break;
+        }
+    }
+    
     return 0;
 }
